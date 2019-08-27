9ac504e6eb066bf439aeb74d8ff9802ccc8f7013
Greg Doud <gpdoud@gmail.com>
2019-08-27 12:27:54 -0400

    Reviewed the while, for, and foreach statements

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -2,9 +2,9 @@
 
 namespace HelloWorldProject {
 
-    class Program { 
+    class Program {  
 
-        static void Main(string[] args) {
+        static void Main(string[] args) { 
             Console.Write("Hello C# Boot Camp World!");
             Console.WriteLine("At MAX Technical Training");
             int daysInBootcamp = 6;
@@ -13,11 +13,46 @@ namespace HelloWorldProject {
             } else {
                 Console.WriteLine("No");
             }
+            //int i = 1;
+            //int j = 0;
+            //int k = i / j;
             string instructor = "";
             instructor = "Greg";
             Console.WriteLine(instructor);
             string lastname = "Doud"; // my lastname
             Console.WriteLine(instructor + " " + lastname);
+            // looping with while
+            var ints = new int[] { 1, 2, 3, 4, 5 };
+            var total = 0;
+            var index = 0;
+            while(index < 5) {
+                total = total + (ints[index] * ints[index]);
+                index = index + 1;
+            }
+            Console.WriteLine("Value is " + total);
+            // looping with for()
+            total = 0;
+            for(var idx = 0; idx < 5; idx = idx + 1) {
+                total = total + (ints[idx] * ints[idx]);
+            }
+            Console.WriteLine("Value is " + total);
+
+
+            var scores = new int[] { 738, 609, 307, 959, 805, 689, 402, 972, 359, 140 };
+            #region assignment compute the average
+            var tot = 0;
+            for(var idx = 0; idx < scores.Length; idx++) {
+                tot = tot + scores[idx];
+            }
+            var avg = tot / scores.Length ;
+            Console.WriteLine("Average is " + avg);
+            #endregion
+
+            // looping with foreach()
+            var grandTotal = 0;
+            foreach(var score in scores) {
+                grandTotal += score;
+            }
         }
     }
 }
