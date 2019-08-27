d0282e5e10de6741a1c312397954c5f2ce14d450
Greg Doud <gpdoud@gmail.com>
2019-08-27 09:15:40 -0400

    Add project files.

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorldProject {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.Write("Hello C# Boot Camp World!");
+            Console.WriteLine("At MAX Technical Training");
+            int daysInBootcamp = 6;
+            if(daysInBootcamp == 6) {
+                Console.WriteLine("Yes");
+            } else {
+                Console.WriteLine("No");
+            }
+            string instructor = "";
+            instructor = "Greg";
+            Console.WriteLine(instructor);
+            string lastname = "Doud"; // my lastname
+            Console.WriteLine(instructor + " " + lastname);
+        }
+    }
+}
